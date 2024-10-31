ccf51244409e4585f77957a7a32ad74050faa352
phungquangthanh2006 <pqthanh.24174600111@sv.uneti.edu.vn>
2024-10-31 18:36:49 +0700

    push bai tap

--- bai_tap_ve_nha_cau_lenh_dieu_kien/bai7.py
+++ bai_tap_ve_nha_cau_lenh_dieu_kien/bai7.py
@@ -6,3 +6,14 @@ print("nhap phuong trinh 2:")
 a2 = float(input("nhap a2: "))
 b2 = float(input("nhap b2: "))
 c2 = float(input("nhap c2: "))
+if a1/a2 == b1/b2:
+    if a1/a2 == b1/b2 != c1/c2:
+       print("he phuong trinh tren vo nghiem")
+    elif a1/a1 == b1/b2 == c1/c2:
+       print("he phuong trinh vo so nghiem")
+else:
+    x = (b1*c2 - b2*c1) / (a2*b1 - b2*a1)
+    print(f"he phuong trinh tren co nghiem x= {x}")
+    y = (c1 - a1*x) / b1
+    print(f"he phuong trinh tren co nghiem y= {y}")
+
